b3c40c87d967bc53c08d13d6094f9bb4905605fb
imamelihuseynov <imamelihuseynov00@gmail.com>
2026-06-06 13:03:51 +0000

    add files

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    try:
+        with open('items.json', 'r') as file:
+            data = json.load(file)
+            item_list = data.get("items", [])
+    except Exception as e:
+        item_list = []
+    
+    return render_template('items.html', items=item_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
